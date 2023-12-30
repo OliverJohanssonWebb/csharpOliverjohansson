66a05e04cb250de5fb58753156e1db51ab26a1f3
Oliver Johansson <98oliver.johansson@gmail.com>
2023-12-30 13:39:43 +0100

    Add project files.

---
+++ ConsoleinlOJ/Users/User.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleinlOJ.Users;
+
+internal class User
+{
+    public string Username { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; } 
+    public string Postalcode { get; set; }
+    public string Phone { get; set; }
+
+
+}
