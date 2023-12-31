17606a82235a4d65842965ab8afde420dd558a73
Oliver Johansson <98oliver.johansson@gmail.com>
2023-12-31 12:41:51 +0100

    Test klart. Project klart.

---
+++ ConsoleinIOJ.Tests/TestClass.cs
@@ -0,0 +1,47 @@
+﻿using ConsoleinlOJ.Users;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+public class UserTests
+{
+    [TestMethod]
+    public void Email_PropertyShouldNotBeNull()
+    {
+        // Arrange
+        User user = new User();
+
+        // Act
+        // Setting Email to null to trigger the null-coalescing behavior
+        user.Email = null;
+
+        // Assert
+        Assert.IsNotNull(user.Email);
+    }
+
+    [TestMethod]
+    public void Email_PropertyShouldReturnEmptyStringWhenSetToNull()
+    {
+        // Arrange
+        User user = new User();
+
+        // Act
+        user.Email = null;
+
+        // Assert
+        Assert.AreEqual(string.Empty, user.Email);
+    }
+
+    [TestMethod]
+    public void Email_PropertyShouldReturnAssignedValue()
+    {
+        // Arrange
+        User user = new User();
+
+        // Act
+        string expectedEmail = "test@example.com";
+        user.Email = expectedEmail;
+
+        // Assert
+        Assert.AreEqual(expectedEmail, user.Email);
+    }
+}
---
+++ TestProject1/Test.cs
@@ -0,0 +1,47 @@
+﻿using ConsoleinlOJ.Users;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+public class UserTests
+{
+    [TestMethod]
+    public void Email_PropertyShouldNotBeNull()
+    {
+        //arrange.
+        User user = new User();
+
+        //act
+        //setting email to null.
+        user.Email = null;
+
+        //assert
+        Assert.IsNotNull(user.Email);
+    }
+
+    [TestMethod]
+    public void Email_PropertyShouldReturnEmptyStringWhenSetToNull()
+    {
+        //Arrange
+        User user = new User();
+
+        //Act
+        user.Email = null;
+
+        //assert
+        Assert.AreEqual(string.Empty, user.Email);
+    }
+
+    [TestMethod]
+    public void Email_PropertyShouldReturnAssignedValue()
+    {
+        //Arrange
+        User user = new User();
+
+        //Act
+        string expectedEmail = "test@example.com";
+        user.Email = expectedEmail;
+
+        //Assert
+        Assert.AreEqual(expectedEmail, user.Email);
+    }
+}
